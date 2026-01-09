6832e6f7e5ee5682b882f25c3c476a0010fd4b9b
PBonvang <peter.bonvang@gmail.com>
2026-01-09 23:32:50 +0100

    Tests/Data: Dummy test removed

--- tests/test_data.py
+++ tests/test_data.py
@@ -1,9 +1,3 @@
 from torch.utils.data import Dataset
 
 from diabetic_classification.data import DiabetesHealthDataset
-
-
-def test_my_dataset():
-    """Test the MyDataset class."""
-    dataset = DiabetesHealthDataset("data/raw")
-    assert isinstance(dataset, Dataset)
